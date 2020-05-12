d543dbcfc5eacf64f52179bf6236aa6ce78614ad
hamza <hz.mouddene@gmail.com>
2020-05-13 01:23:53 +0200

    release

--- 1A/S6/Architecture/TPs/TP5_6/event_IT.asm
+++ 1A/S6/Architecture/TPs/TP5_6/event_IT.asm
@@ -8,10 +8,12 @@
 
 LD = 0xB0000000                                             // leds I/O address  
 MEM = 0x100                                                 // Memory address 0x100   
+
 ba main                                                     // branch always main
 
 handler:                                                    // Interruption subprogram  
-    inc %r3                                                 // %r3 += 1
+    inc %r2                                                 // %r2 += 1
+    st %r2, [%r3]                                           // MEM = %r2
     reti                                                    // Goto fetch
     
 main:
@@ -19,9 +21,10 @@ main:
 
     set LD, %r9                                             // %r9 = &LD
     set MEM, %r3                                            // %r3 = &MEM
-    clr %r3                                                 // MEM = 0
+    st %r0, [%r3]                                           // MEM = 0
 
     loop:                                                   // Start loop                                                     
-         st %r3, [%r9]                                      // Store %r3 on the leds     
+         ld [%r3], %r2                                      // %r2 = MEM
+         st %r2, [%r9]                                      // Store %r2 on the leds     
     ba loop                                                 // Alawys loop
 /** End of program */
