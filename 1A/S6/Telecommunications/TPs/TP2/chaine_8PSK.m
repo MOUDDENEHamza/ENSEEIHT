63d74424041dfbb3cf05fb5d5bc50d7499e132fb
hamza <hz.mouddene@gmail.com>
2020-05-12 03:22:36 +0200

    release

--- 1A/S6/Telecommunications/TPs/TP2/chaine_8PSK.m
+++ 1A/S6/Telecommunications/TPs/TP2/chaine_8PSK.m
@@ -1,7 +1,7 @@
 %--------------------------------------------------------------------------
 % ENSEEIHT - 1SN - Télécommunications
 % TP2 - Etudes de chaines de transmission sur fréquence porteuse
-% chaine_8_PSK.m
+% chaine_8PSK.m
 %--------------------------------------------------------------------------
 
 %--------------------------------------------------------------------------
@@ -18,7 +18,6 @@ clc;
 %--------------------------------------------------------------------------
 
 % Initialisation des paramètres
-% Initialisation des paramètres
 Fe = 10000;
 Te = 1 / Fe;
 alpha = 0.35;
@@ -26,28 +25,28 @@ span = 8;
 fp = 2000;
 fc = 1500;
 Rs = 3000;
-nb_bits = 1000;
+nb_bits = 10000;
 Ns = floor(Fe / Rs);
-N = 50;
+M = 8;
 
 %%
 %--------------------------------------------------------------------------
 % Implantation de la chaine sans bruit
 %--------------------------------------------------------------------------
 % Génération de l’information binaire
-bits = randi([0, 1], 1, nb_bits);
+bits = randi([0, M - 1], 1, nb_bits);
 
-% Mapping permettant d'obtenir dk ∈ {±1 ± j}
-ak = 2 * bits(1 : 2 : end) - 1;
-bk = 2 * bits(2 : 2 : end) - 1;
-dk = ak + 1j * bk;
+% Mapping permettant d'obtenir dk
+dk = pskmod(bits, M, pi / M);
+ak = real(dk);
+bk = imag(dk);
 
 % Génération de la suite de Diracs pondérés par les symbols (suréchantillonnage)
 Suite_diracs1 = kron(ak, [1 zeros(1, Ns - 1)]);
 Suite_diracs2 = kron(bk, [1 zeros(1, Ns - 1)]);
 
 % Génération de la réponse impulsionnelle du filtre de mise en forme
-h = rcosdesign(alpha, 8, Ns, 'sqrt');
+h = rcosdesign(alpha, span, Ns, 'sqrt');
 retard = (span * Ns) / 2;
 
 % Filtrage de mise en forme du signal généré sur la voie I
@@ -58,9 +57,9 @@ I = I(retard + 1 : end);
 Q = filter(h, 1, [Suite_diracs2 zeros(1, retard)]);
 Q = Q(retard + 1 : end);
 
-% L’enveloppe complexe associée au signal transmis modulé
+% Le signal transmis sur fréquence porteuse
 T = [0 : length(I) - 1] * Te;
-x_e =  I + 1i * Q;
+x =  I .* cos(2 * pi * fp * T) - Q .* sin(2 * pi * fp * T);
 
 % Affichage du signal génerée sur la voie en phase
 figure ;
@@ -78,32 +77,31 @@ title('Signal généré sur la voie en quadrature Q');
 xlabel('Temps en secondes');
 ylabel('Q(t)');
 
+% Affichage du signal transmis
+figure ;
+plot(x);
+axis([0 nb_bits - 1 -1 1]);
+title('Signal émis x sans bruit');
+xlabel('Temps en secondes');
+ylabel('Signal émis x');
 
 %%
-% Calcul de la DSP  de l’enveloppe complexe associée au signal transmis 
-% modulé sur fréquence porteuse par périodogramme
-DSP_x_e = (1 / length(x_e)) * abs(fft(x_e, 2 ^ nextpow2(length(x_e)))) .^ 2;
+% Retour en bande de base
+signal_I = x .* cos(2 * pi * fp * T);
+signal_Q = -j * x .* sin(2 * pi * fp * T);
+y = signal_I + signal_Q;
 
-% Affichage de la DSP de l’enveloppe complexe associée au signal transmis 
-% modulé sur fréquence porteuse par périodogramme
-figure;
-plot(linspace(-Fe / 2, Fe / 2, length(DSP_x_e)), fftshift(DSP_x_e));
-title('DSP de l’enveloppe complexe associée au signal transmis modulé');
-xlabel('Fréquences en Hz');
-ylabel('S_{x_{e}}(f)');
-
-%%
 % Génération de la réponse impulsionnelle du filtre de réception
 h_r = h;
 
 % Filtrage de réception
-z = filter(h_r, 1, [x_e zeros(1,retard)]);
+z = filter(h_r, 1, [y zeros(1,retard)]);
 z = z(retard + 1 : end);
 
 % Affichage du signal reçu
 figure ;
 plot(real(z));
-axis([0 nb_bits - 1 -1.5 1.5]);
+axis([0 nb_bits - 1 -1 1]);
 title('Figure 1 : signal reçu z sans bruit');
 xlabel('Temps en secondes');
 ylabel('Signal reçu z');
@@ -119,40 +117,32 @@ xlabel('Temps en secondes');
 z_echant = z(1 : Ns : end);
 
 % Detecteur à seuil
-for i = 1 : length(z_echant)
-    if (real(z_echant(i)) <= 0 && imag(z_echant(i)) <= 0)
-        symboles_decides(i) = -1 - 1i;
-        
-    elseif (real(z_echant(i)) >= 0 && imag(z_echant(i)) >= 0)
-        symboles_decides(i) = 1 + 1i;
-        
-    elseif (real(z_echant(i)) <= 0 && imag(z_echant(i)) >= 0)
-        symboles_decides(i) = -1 + 1i;
-        
-    elseif (real(z_echant(i)) >= 0 && imag(z_echant(i)) <= 0)
-        symboles_decides(i) = 1 - 1i;
-    end
-end
-
-% Calcul du TES
-TES = length(find(symboles_decides ~= dk)) / (length(dk));
+bits_decides = pskdemod(z_echant, M, pi / M);
 
 % Calcul du TEB
-TEB = TES / log2(4)
+TEB = length(find(bits_decides ~= bits)) / length(bits)
 
 %%
 %--------------------------------------------------------------------------
 % Implantation de la chaine avec bruit
 %--------------------------------------------------------------------------
+%%
+
 TES = zeros(1,7);
 TEB = zeros(1,7);
 
 for i = 0 : 6
     % L'ajout du bruit blanc gaussien
-    Puiss_sign = mean(abs(x_e) .^ 2);
-    Puiss_bruit = Puiss_sign * Ns  / (2 * log2(4) * 10 .^ (i / 10));
-    Bruit_gauss = (sqrt(Puiss_bruit) * randn(1, length(x_e))) + 1i * (sqrt(Puiss_bruit) * randn(1, length(x_e)));
-    y = x_e + Bruit_gauss; 
+    N = randn(1, length(x));
+    Puiss_sign = mean(abs(x) .^ 2);
+    Puiss_bruit = Puiss_sign * Ns  / (2 * log2(M) * 10 .^ (i / 10));
+    Bruit_gauss = sqrt(Puiss_bruit) * N;
+    y = x + Bruit_gauss;
+    
+    % Retour en bande de base
+    signal_I = y .* cos(2 * pi * fp * T);
+    signal_Q = -1i * y .* sin(2 * pi * fp * T);
+    y = signal_I + signal_Q;
     
     % Génération de la réponse impulsionnelle du filtre de réception
     h_r = h;
@@ -174,34 +164,11 @@ for i = 0 : 6
     ylabel('Q');
     
     % Detecteur à seuil
-    for j = 1 : length(z_echant)
-        if (real(z_echant(j)) <= 0 && imag(z_echant(j)) <= 0)
-            symboles_decides(j) = -1 - 1i;
-        elseif (real(z_echant(j)) >= 0 && imag(z_echant(j)) >= 0)
-            symboles_decides(j) = 1 + 1i;
-        elseif (real(z_echant(j)) <= 0 && imag(z_echant(j)) >= 0)
-            symboles_decides(j) = -1 + 1i;
-        elseif (real(z_echant(j)) >= 0 && imag(z_echant(j)) <= 0)
-            symboles_decides(j) = 1 - 1i;
-        end
-    end
-    
-    % Calcul du TES
-    TES(i + 1) = length(find(symboles_decides ~= dk)) / (length(dk));
+    bits_decides = pskdemod(z_echant, M, pi / M);
     
     % Calcul du TEB
-    TEB(i + 1) = TES(i + 1) / 2;
+    TEB(i + 1) = length(find(bits_decides ~= bits)) / length(bits);
 end
-
-%%
-% Comparaison entre le taux d’erreur binaire (TEB) obtenu en fonction Eb/N0
-% et le TEB théorique
-figure;
-semilogy([0 : 6], TEB);
-grid
-title('TEB');
-xlabel("$\frac{Eb}{N_{o}}$ (dB)", 'Interpreter', 'latex');
-ylabel('TEB');
 
 %%
 % Comparaison entre le taux d’erreur binaire (TEB) obtenu en fonction Eb/N0
@@ -209,9 +176,9 @@ ylabel('TEB');
 figure;
 semilogy([0 : 6], TEB, 'r*');
 hold on
-semilogy([0 : 6], (4 * (1 - (1 / sqrt(4))) * qfunc(sqrt(3 * log2(4)* 10 .^ ([0 : 6] / 10) / (3)))) / log2(4));
+semilogy([0 : 6], 2 * qfunc(sqrt(2 * log2(M) * 10 .^ ([0 : 6] / 10)) * sin(pi / M)));
 grid
-title('Comparaison entre le TEB théorique et estimé');
+title('Figure 8 : Comparaison entre le TEB théorique et estimé');
 legend('TEB estimé','TEB théorique')
 xlabel("$\frac{Eb}{N_{o}}$ (dB)", 'Interpreter', 'latex');
 ylabel('TEB');
