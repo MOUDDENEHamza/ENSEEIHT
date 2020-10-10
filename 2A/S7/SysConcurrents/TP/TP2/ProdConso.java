26388bbaa0ec67e2d76fb33f6e8014dede4efd84
MOUDDENEHamza <hz.mouddene@gmail.com>
2020-10-10 19:30:45 +0200

    TP2 SYSCON

--- 2A/S7/SysConcurrents/TP/TP2/ProdConso.java
+++ 2A/S7/SysConcurrents/TP/TP2/ProdConso.java
@@ -1,89 +1,98 @@
 // v0.1, 25/09/20 (PM)
 
 class TamponBorné {
-/* Pour simplifier la mise en œuvre du test, on se limite ici à un tampon d'entiers */
-    private int taille; 			// nombre de cases du tampon
+    /* Pour simplifier la mise en œuvre du test, on se limite ici à un tampon d'entiers */
+    private int taille;            // nombre de cases du tampon
     private int nbOccupé = 0; // nombre d'items présents dans le tampon (vide initialement)
-    private int queue = 0; 		// queue de file (tampon circulaire), insertions
-    private int tête = 0; 		// tête de file (tampon circulaire), extractions
+    private int queue = 0;        // queue de file (tampon circulaire), insertions
+    private int tête = 0;        // tête de file (tampon circulaire), extractions
 
     private int trace = 0;// utile uniquement pour faciliter le test :
-													// on va considérer qu'on dépose les valeurs successives de trace...
+    // on va considérer qu'on dépose les valeurs successives de trace...
 
     private int[] tampon;
+    final Object mutex = new Object();
 
     public TamponBorné(int t) {
         taille = t;
-        tampon = new int [taille];
+        tampon = new int[taille];
     }
 
     public void déposer() { // pour le test, les valeurs déposées sont prédéfinies
 
-/** A compléter : attendre que le dépôt soit possible. 
-  * Pour une attente bloquante, il suffit que le thread courant appelle wait() (ou this.wait())
-  *  (c.-à-d. qu'on peut utiliser le verrou associé au TamponBorné sur lequel on appelle déposer() )
-  *
-  *
-  *
-  *
-  **/
-		if (nbOccupé == taille) {
-        	try {
-	        	Thread.currentThread().wait();
-	        } catch (InterruptedException e)  {
-	            Thread.currentThread().interrupt();
-	        }
-        } else {
-        	// dépôt
-        	tampon[queue] = trace++; 			// tampon[queue] = i;
-        	queue = (queue + 1) % taille;
-        	nbOccupé++;
-		}
-		
-        // affichage pour le test uniquement
-        String msg="P : "+(trace-1);
-        if (nbOccupé == taille) msg=msg+ " (PLEIN)";
-        System.out.println(msg);
+        /** A compléter : attendre que le dépôt soit possible.
+        * Pour une attente bloquante, il suffit que le thread courant appelle wait() (ou this.wait())
+        *  (c.-à-d. qu'on peut utiliser le verrou associé au TamponBorné sur lequel on appelle déposer() )
+        *
+        *
+        *
+        *
+        **/
+        synchronized (this.mutex) {
+            if (nbOccupé == taille) {
+                try {
+                    this.wait();
+                } catch (Exception e) {
+                }
+            } else {
+                // dépôt
+                tampon[queue] = trace++;            // tampon[queue] = i;
+                queue = (queue + 1) % taille;
+                nbOccupé++;
+            }
 
-/** A compléter : réveiller (notify()) un éventuel thread en attente d'accès au TamponBorné
-  *
-  *
-  **/
-  		if (nbOccupé == taille) notify();
+            // affichage pour le test uniquement
+            String msg = "P : " + (trace - 1);
+            if (nbOccupé == taille) msg = msg + " (PLEIN)";
+            System.out.println(msg);
+
+            /** A compléter : réveiller (notify()) un éventuel thread en attente d'accès au TamponBorné
+             *
+             *
+             **/
+            try {
+                notify();
+            } catch (Exception e) {
+            }
+        }
     } //déposer()
 
-    public int retirer() {	// Item remove()
-        int i;							// Item i
+    public int retirer() {    // Item remove()
+        int i;                            // Item i
 
-/** A compléter : attendre que le retrait soit possible
-  *
-  *
-  *
-  *
-  **/
-  		i = tampon[tête];
-		if (nbOccupé == 0) {
-        	try {
-	        	Thread.currentThread().wait();
-	        } catch (InterruptedException e)  {
-	            Thread.currentThread().interrupt();
-	        }
-        } else {
-		    // retrait
-		    tête = (tête + 1) % taille;
-		    nbOccupé--;
-		}
-		
-        // affichage pour le test uniquement
-        String msg= "C : " + i;
-        if (nbOccupé == 0) msg=msg+ " (VIDE)";
-        System.out.println(msg);
+        /** A compléter : attendre que le retrait soit possible
+         *
+         *
+         *
+         *
+         **/
+        synchronized (this.mutex) {
+            i = tampon[tête];
+            if (nbOccupé == 0) {
+                try {
+                    this.wait();
+                } catch (Exception e) {
+                }
+            } else {
+                // retrait
+                tête = (tête + 1) % taille;
+                nbOccupé--;
+            }
 
-/** A compléter : réveiller un éventuel thread en attente d'accès au TamponBorné
-  *
-  *
-  **/
-  		if (nbOccupé == 0) notify();
+            // affichage pour le test uniquement
+            String msg = "C : " + i;
+            if (nbOccupé == 0) msg = msg + " (VIDE)";
+            System.out.println(msg);
+
+            /** A compléter : réveiller un éventuel thread en attente d'accès au TamponBorné
+             *
+             *
+             **/
+            try {
+                notify();
+            } catch (Exception e) {
+            }
+        }
         return i;
     } // retirer
 } // TamponBorné
@@ -92,16 +101,18 @@ class TamponBorné {
 
 class Producteur implements Runnable {
     private TamponBorné tampon;
+
     public Producteur(TamponBorné t) {
         tampon = t;
     }
+
     public void run() {
         try {
-            Thread.sleep(10) ; // pour le test : initialement, les consommateurs trouveront tous un tampon vide
+            Thread.sleep(10); // pour le test : initialement, les consommateurs trouveront tous un tampon vide
             for (int i = 0; i < 25; i++) {
                 // possible de trouver des producteurs bloqués à la fin, selon le nb de consommateurs
                 tampon.déposer();
-                Thread.sleep(2*i); //producteurs ralentissent un peu
+                Thread.sleep(2 * i); //producteurs ralentissent un peu
             }
         } catch (InterruptedException e) {
             System.out.println("interrompu");
@@ -112,16 +123,18 @@ class Producteur implements Runnable {
 class Consommateur implements Runnable {
     private TamponBorné tampon;
     private int identité;
+
     public Consommateur(TamponBorné t) {
         tampon = t;
     }
+
     public void run() {
         int res;
         for (int i = 0; i < 25; i++) {
             // possible de trouver des consommateurs bloqués à la fin, selon le nb de producteurs
             res = tampon.retirer();
             try {
-                Thread.sleep(10*i); //consommateurs ralentissent davantage
+                Thread.sleep(10 * i); //consommateurs ralentissent davantage
             } catch (InterruptedException e) {
                 System.out.println("interrompu");
             }
@@ -137,20 +150,20 @@ public class ProdConso {
         //aucun blindage : on suppose que les valeurs de paramètres fournies sont raisonnables
         if (args.length != 3) {
             System.out.println("java ProdConso <nbProd> <nbConso> <nbCases>");
-            System.out.println("-> choix par défaut : "+nbProd+"/"+nbConso+"/"+tailleTampon);
+            System.out.println("-> choix par défaut : " + nbProd + "/" + nbConso + "/" + tailleTampon);
         } else {
-            nbProd = Integer.parseInt (args[0]);
-            nbConso = Integer.parseInt (args[1]);
-            tailleTampon = Integer.parseInt (args[2]);
+            nbProd = Integer.parseInt(args[0]);
+            nbConso = Integer.parseInt(args[1]);
+            tailleTampon = Integer.parseInt(args[2]);
         }
         System.out.println("nbProd (arg1) : " + nbProd + " /nbConso (arg2) : " + nbConso
-                           + " /nbCases) (arg3) : "+ tailleTampon);
+                + " /nbCases) (arg3) : " + tailleTampon);
         TamponBorné t = new TamponBorné(tailleTampon);
         for (int i = 0; i < nbProd; i++) {
-            new Thread(new Producteur(t)).start() ;
+            new Thread(new Producteur(t)).start();
         }
         for (int i = 0; i < nbConso; i++) {
-            new Thread(new Consommateur(t)).start() ;
+            new Thread(new Consommateur(t)).start();
         }
         //ajouter éventuellement un thread pour gérer l'arrêt et une prise de cliché finale
     }
