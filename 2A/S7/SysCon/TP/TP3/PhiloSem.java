3eb02c62268cdef1227ad4b496c41a080e7f1e51
MOUDDENEHamza <hz.mouddene@gmail.com>
2020-10-22 23:08:07 +0200

    release

--- 2A/S7/SysCon/TP/TP3/PhiloSem.java
+++ 2A/S7/SysCon/TP/TP3/PhiloSem.java
@@ -4,23 +4,34 @@ import java.util.concurrent.Semaphore;
 
 public class PhiloSem implements StrategiePhilo {
 
-    /****************************************************************/
+    /** Attribut de classe */
+    private static Semaphore[] fourchettes;
 
+    /** Constructeur de classe */
     public PhiloSem (int nbPhilosophes) {
+        this.fourchettes = new Semaphore[nbPhilosophes];
+        for (int i = 0; i < nbPhilosophes; i++)
+            this.fourchettes[i] = new Semaphore(1);
     }
 
     /** Le philosophe no demande les fourchettes.
      *  Précondition : il n'en possède aucune.
      *  Postcondition : quand cette méthode retourne, il possède les deux fourchettes adjacentes à son assiette. */
-    public void demanderFourchettes (int no) throws InterruptedException
-    {
+    public void demanderFourchettes (int no) throws InterruptedException{
+        this.fourchettes[Main.FourchetteDroite(no)].acquire();
+        IHMPhilo.poser(Main.FourchetteDroite(no), EtatFourchette.AssietteGauche);
+        this.fourchettes[Main.FourchetteGauche(no)].acquire();
+        IHMPhilo.poser(Main.FourchetteGauche(no), EtatFourchette.AssietteDroite);
     }
 
     /** Le philosophe no rend les fourchettes.
      *  Précondition : il possède les deux fourchettes adjacentes à son assiette.
      *  Postcondition : il n'en possède aucune. Les fourchettes peuvent être libres ou réattribuées à un autre philosophe. */
-    public void libererFourchettes (int no)
-    {
+    public void libererFourchettes (int no) {
+        this.fourchettes[Main.FourchetteDroite(no)].release();
+        IHMPhilo.poser(Main.FourchetteDroite(no), EtatFourchette.Table);
+        this.fourchettes[Main.FourchetteGauche(no)].release();
+        IHMPhilo.poser(Main.FourchetteGauche(no), EtatFourchette.Table);
     }
 
     /** Nom de cette stratégie (pour la fenêtre d'affichage). */
