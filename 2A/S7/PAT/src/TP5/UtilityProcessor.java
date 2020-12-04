c3fc01d7a47e729e92cbc9a4277ffa0d2a48975e
MOUDDENEHamza <hz.mouddene@gmail.com>
2020-12-04 12:55:27 +0100

    # : TP5 in progress

--- 2A/S7/PAT/src/TP5/UtilityProcessor.java
+++ 2A/S7/PAT/src/TP5/UtilityProcessor.java
@@ -6,7 +6,9 @@ import javax.lang.model.element.*;
 import java.util.*;
 import javax.tools.Diagnostic.Kind;
 
-/** Check that a class marked {@code @Utility} is indeed a utility class. */
+/**
+ * Check that a class marked {@code @Utility} is indeed a utility class.
+ */
 @SupportedAnnotationTypes("Utility")
 @SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class UtilityProcessor extends AbstractProcessor {
@@ -14,8 +16,7 @@ public class UtilityProcessor extends AbstractProcessor {
     @Override
     public boolean process(
             Set<? extends TypeElement> annotations,
-            RoundEnvironment roundingEnvironment)
-    {
+            RoundEnvironment roundingEnvironment) {
         Messager messager = processingEnv.getMessager();
         messager.printMessage(Kind.NOTE,
                 "UtilityProcessor executed.");
@@ -24,17 +25,29 @@ public class UtilityProcessor extends AbstractProcessor {
         for (TypeElement te : annotations) {
             for (Element elt : roundingEnvironment.getElementsAnnotatedWith(te)) {
                 if (elt.getKind() != ElementKind.CLASS) {    // elt is a class
-                    messager.printMessage(Kind.WARNING, "This is not a class");
+                    messager.printMessage(Kind.WARNING, "This is not a class", elt);
                 } else {
                     boolean isFinal = elt.getModifiers().contains(Modifier.FINAL);
-                    List<Element> elementList = (List<Element>) elt.getEnclosingElement();
-                    // Check that the class is declared final
-
+                    if (!(isFinal)) { // If the class is not final
+                        messager.printMessage(Kind.WARNING, "this class is not final");
+                    }
+                    // If the class is final
                     // Check that enclosed elements are static
-                } else {
-                    messager.printMessage(Kind.ERROR,
-                            "@Utility applies to class only:", elt);
-                }
+                    boolean isStatic = true;
+                    List<Element> elementList = (List<Element>) elt.getEnclosingElement();
+                    for (Element e : elementList) {
+                        if (!e.getModifiers().contains(Modifier.STATIC)) {
+                            isStatic = false;
+                            break;
+                        }
+                    }
+                    if (!isStatic) {
+                        messager.printMessage(Kind.WARNING, "There is an enclosed element that is not static", elt);
+                    } else {
+                        messager.printMessage(Kind.ERROR,
+                                "@Utility applies to class only:", elt);
+                    }
+
                 }
             }
         }
