b285fd7c677ed52abac463d7c7de1dd01cb9601d
MOUDDENEHamza <hz.mouddene@gmail.com>
2020-12-17 02:47:58 +0100

    #PDR : test hidoop

--- 2A/S7/PDR/src/Main.java
+++ 2A/S7/PDR/src/Main.java
@@ -1,21 +1,19 @@
-import hdfs.HdfsServer;
 import hdfs.NameProvider;
-import hdfs.Register;
 import ordo.WorkerImpl;
 
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 
 public class Main {
 
     public static void main(String[] args) throws RemoteException, UnknownHostException, NotBoundException {
         String[] arg = {"8000"};
         WorkerImpl.main(arg);
+        System.out.println("\n\n");
+        System.out.println("\n\n");
         NameProvider np = new NameProvider();
-        HdfsServer server = new HdfsServer(
-                (Register) LocateRegistry.getRegistry(NameProvider.NAME_PROVIDER_PORT).lookup("//localhost:" + NameProvider.NAME_PROVIDER_PORT + "/ServerRegistry"),
-                "serverData", "server");
+        String[] args1 = {"server"};
+
     }
 }
