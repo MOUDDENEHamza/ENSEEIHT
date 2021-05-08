6cc1acf53ca1f2af6b675aa4a95a50f1d6a4e93b
hamza <hz.mouddene@gmail.com>
2021-05-08 21:25:55 +0200

    #

--- 2A/S8/PAT/TP4/comptes/ExempleComptes.java
+++ 2A/S8/PAT/TP4/comptes/ExempleComptes.java
@@ -1,5 +1,29 @@
 /** Programme utilisant les comptes bancaires.  */
 public class ExempleComptes {
+	
+	pointcut publicMethod():
+	call(void CompteCourant.*(double)) ||
+	call(void CompteCourant.debiter(double)) ||
+	call(void CompteCourant.editerReleve()) ||
+	call(String CompteCourant.toString()) ||
+	call (String CompteSimple.getNumero()) ||
+	call (double CompteSimple.getSolde()) ||
+	call (Personne CompteSimple.getTitulaire()) ||
+	call(void CompteSimple.crediter(double)) ||
+	call(void CompteSimple.debiter(double)) ||
+	call(String CompteSimple.toString())
+	/**
+	pointcut publicMethod():
+	call(public CompteCourant.*(..)) ||
+	call (public CompteSimple.*(..)) 
+	 */
+
+	before() : publicMethod() {
+		System.out.println(thisJoinPoint.getSignature() + "   " + thisJoinPoint.getArgs());
+	}
+
+
+
 	public static void main (String argv []) {
 		Personne p1 = new Personne("Xavier", "Crégut", true);
 		CompteSimple cs1 = new CompteSimple(p1, 0);
