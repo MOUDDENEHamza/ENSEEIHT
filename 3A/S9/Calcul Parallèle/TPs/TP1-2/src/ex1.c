be1ba85d54316df43f32a42faa25d3f1e994166e
hamza <hz.mouddene@gmail.com>
2021-11-16 13:22:11 +0100

    #Start TP1-2

--- 3A/S9/Calcul Parallèle/TPs/TP1-2/src/ex1.c
+++ 3A/S9/Calcul Parallèle/TPs/TP1-2/src/ex1.c
@@ -19,11 +19,18 @@ void p2p_transmit_A(int p, int q, Matrix *A, int i, int l) {
     Ail =  & A->blocks[i][l];
     b = A->b;
     /* TODO : transmit A[i,l] using MPI_Ssend & MPI_Recv */
-    if (1 /* I own A[i,l]*/) {
+    if (Ail->owner == me) {/* I own A[i,l]*/
       /* MPI_Ssend A[i,l] to my row */
-    } else if (0 /* A[i,l] is stored on my row */) {
-      Ail->c = malloc(b*b*sizeof(float));
+      for (j = 0; j < q; j++) {
+        node = get_node(p, q, i, j);
+        if (node != me) {
+          MPI_Ssend(Ail->c, b * b, MPI_FLOAT, node, tag, MPI_COMM_WORLD);
+        }
+      }
+    } else if (my_row == Ail->row) { /* A[i,l] is stored on my row */
+      Ail->c = malloc(b * b * sizeof(float));
       /* MPI_Recv A[i,l] */
+      MPI_Recv(Ail->c, b * b, MPI_FLOAT, Ail->owner, tag, MPI_COMM_WORLD, &status);
     }
     /* end TODO */
 }
@@ -31,9 +38,7 @@ void p2p_transmit_A(int p, int q, Matrix *A, int i, int l) {
 void p2p_transmit_B(int p, int q, Matrix *B, int l, int j) {
     int i;
     int me, my_row, my_col;
-    MPI_Status status;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+      if (my_col == Blj->col) {
     node_coordinates_2i(p,q,me,&my_row,&my_col);
 
     int node, tag, b;
@@ -41,11 +46,19 @@ void p2p_transmit_B(int p, int q, Matrix *B, int l, int j) {
     Blj =  & B->blocks[l][j];
     b = B->b;
     /* TODO : transmit B[l,j] using MPI_Ssend & MPI_Recv */
-    if (1 /* I owned B[l,j]*/) { 
+    if (Blj->owner == me /* I owned B[l,j]*/) { 
       /* MPI_Ssend B[l,j] to my column */
-    } else if (0 /* B[l,j] is stored on my column */) {
+      for (i = 0; i < p; i++) {
+        node = get_node(p, q, i, j);
+        if (node != me) {
+          MPI_Ssend(Blj->c, b * b, MPI_FLOAT, node, tag, MPI_COMM_WORLD);
+        }
+      }
+    } else if (my_col == Blj->col /* B[l,j] is stored on my column */) {
       Blj->c = malloc(b*b*sizeof(float));
+      if (my_col == Blj->col) {
       /* MPI_Recv B[l,j] */
+      MPI_Recv(Blj->c, b * b, MPI_FLOAT, Blj->owner, tag, MPI_COMM_WORLD, &status);
     }
     /* end TODO */
 }
